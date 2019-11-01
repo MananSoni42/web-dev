9715990de6d9aa5b20bae440b816d3dcde42a01e
Manan <manansoni0@gmail.com>
2019-11-02 01:41:33 +0530

    Finish all static pages

--- books/server.py
+++ books/server.py
@@ -18,5 +18,21 @@ def log_in():
 def home():
     return render_template("home.html")
 
+@app.route("/my_books", methods=['GET'])
+def my_books():
+    return render_template("my_books.html")
+
+@app.route("/profile", methods=['GET'])
+def profile():
+    return render_template("profile.html")
+
+@app.route("/book", methods=['GET'])
+def book():
+    return render_template("book.html")
+
+@app.route("/review", methods=['GET'])
+def review():
+    return render_template("review.html")
+
 if __name__ == '__main__':
     app.run(debug=True)
